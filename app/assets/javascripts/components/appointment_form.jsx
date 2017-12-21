791dd31e9510125320452960e15f8191a75f18c3
ileeds <ileeds@brandeis.edu>
2017-12-20 22:35:43 -0500

    deploy

---
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -0,0 +1,39 @@
+window.AppointmentForm = createReactClass({
+  handleChange: function(e) {
+    var name = e.target.name;
+    obj = {}
+    obj[name] = e.target.value;
+    this.props.onUserInput(obj);
+  },
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+    this.props.onFormSubmit();
+  },
+
+  setApptTime: function(e) {
+    var name = 'appt_time';
+    var obj = {};
+    if (obj[name] = e.toDate()) {
+      this.props.onUserInput(obj);
+    }
+  },
+
+  render: function() {
+    var inputProps = {
+      name: 'appt_time'
+    };
+    return (
+      <div>
+        <h2>Make a new appointment</h2>
+        <form onSubmit={this.handleSubmit}>
+          <input name='title' placeholder='Appointment title'
+            value={this.props.title} onChange={this.handleChange} />
+          <Datetime input={false} open={true} inputProps={inputProps}
+            value={this.props.appt_time} onChange={this.setApptTime}/>
+          <input type='submit' value='Make appointment' className='submit-button' />
+        </form>
+      </div>
+    )
+  }
+});
---
+++ app/assets/javascripts/components/appointments_list.jsx
@@ -0,0 +1,13 @@
+window.AppointmentsList = createReactClass({
+  render: function() {
+    return (
+      <div>
+        {this.props.appointments.map(function(appointment) {
+          return(
+            <Appointment appointment={appointment} key={appointment.id} />
+          )
+        })}
+      </div>
+    )
+  }
+});
