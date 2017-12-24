1bc46f29612a61d9c845aa35227995f0bc84d936
ileeds <ileeds@brandeis.edu>
2017-12-24 18:47:08 -0500

    migrate to react on rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,11 +12,6 @@
 //
 //= require rails-ujs
 //= require turbolinks
-//= require react
-//= require react_ujs
 //= require jquery
 //= require jquery_ujs
-//= require moment.min
-//= require react-datetime.min
-//= require components
 //= require_tree .
